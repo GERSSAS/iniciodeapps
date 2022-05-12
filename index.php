f1712addff5482056630de7bcb7d0ccc85fc27e6
IvanAlexisUrbina <alexis-crokis@hotmail.com>
2022-05-12 08:31:11 -0500

    agregado

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>APPS GERS</title>
+    <!-- Fuentes -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,900&display=swap" rel="stylesheet">
+    <!-- Estilos CSS -->
+    <link rel="stylesheet" href="./css/estilos.css">
+    <link rel="shorcut icon" href="img/Gers.ico">
+</head>
+
+<body>
+    <!-- ID Particles.js -->
+    <div id="particles-js"></div>
+    <header class="contenedor header">
+
+
+        <img id="logo" src="img/Gers.png" alt="">
+
+
+        <!-- Título y descripción -->
+        <div class="contenido">
+
+            <div class="items">
+
+                    <img src="img/hojas.png" alt="">
+                <div class="capa"><label for="">Cotizaciones</label></div>
+                <div class="capa2"><label for="">Cotizaciones</label><br><p>-Es un aplicativo que gestiona procesos internos de la empresa.</p></div>
+               
+            </div>
+
+            <div class="items">
+                <img src="img/vacantes.png" alt="">
+                <div class="capa"><label for="">Recursos humanos</label></div>
+                <div class="capa2"><label for="">Recursos humanos</label><br><p>-Es un aplicación del area de recursos humanos que se encarga de la contratacion para nuevo personal de la empresa.</p></div>
+                
+
+            </div>
+            
+
+        </div>
+
+
+    </header>
+
+    <!-- JS Particles.js -->
+    <script src="js/particles.min.js"></script>
+    <script src="js/app.js"></script>
+
+    <!-- JS FontAwesome -->
+    <script src="https://kit.fontawesome.com/a2e8d0339c.js"></script>
+</body>
+
+</html>
